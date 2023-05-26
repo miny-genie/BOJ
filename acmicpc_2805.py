8efd1dd6078a458ebcc96911c741c4d9abe32f33
김지민 <kjmin2000@naver.com>
2023-05-26 22:53:22 +0900

    BOJ

---
+++ acmicpc_2805.py
@@ -0,0 +1,30 @@
+# ---------- Import ----------
+import sys
+input = sys.stdin.readline
+
+# ---------- Main ----------
+treeCnt, treeLength = map(int, input().split())
+trees = list(map(int, input().split()))
+
+start, end = 0, max(trees)
+while start <= end:
+    mid = (start + end) // 2
+
+    totalLength = 0
+    for tree in trees:
+        remain = tree - mid
+        if remain > 0:
+            totalLength += remain
+
+    # length is enough, so cut higher
+    if totalLength > treeLength:
+        start = mid + 1
+
+    # length is not enough, so cut lower
+    elif totalLength < treeLength:
+        end = mid - 1
+
+    else:
+        break
+
+print(mid)
