d72aea79d38b7474d84cdb910f25d7891883d368
miny_genie <kjmin2000@naver.com>
2023-08-09 17:59:47 +0900

    BOJ

--- acmicpc_15686.py
+++ acmicpc_15686.py
@@ -11,18 +11,22 @@ city_chicken_distance = 1e9
 houses = [(x, y) for x in range(N) for y in range(N) if city[x][y] == 1]
 chickens = [(x, y) for x in range(N) for y in range(N) if city[x][y] == 2]
 
+# Select chicken place using combinations
 for matrix in combinations(chickens, K):
     tmp = 0
     
+    # Brute force every house
     for hx, hy in houses:
         chicken_distance = 1e9
         
+        # Calculating chicken distance
         for cx, cy in matrix:
             distance = abs(hx-cx) + abs(hy-cy)
             chicken_distance = min(chicken_distance, distance)
             
         tmp += chicken_distance
     
+    # Compare chicken distance
     city_chicken_distance = min(city_chicken_distance, tmp)
         
 print(city_chicken_distance)
