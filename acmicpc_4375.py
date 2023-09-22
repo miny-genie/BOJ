e299da90eb1b5b8352dea025a675f53e7a024b13
miny_genie <kjmin2000@naver.com>
2023-09-23 01:36:39 +0900

    BOJ

--- acmicpc_4375.py
+++ acmicpc_4375.py
@@ -1,3 +1,4 @@
+# -------------------- Case 1 --------------------
 # ---------- Main ----------
 try:
     while True:
@@ -12,6 +13,28 @@ try:
                 
             else:
                 isMulN += "1"
+
+except:
+    exit()
+
+
+# -------------------- Case 2 --------------------
+# ---------- Main ----------
+try:
+    while True:
+        N = int(input())
+
+        isMulN = 1
+        length = 1
+        
+        while True:
+            if isMulN % N == 0:
+                print(length)
+                break
+                
+            else:
+                isMulN = (isMulN * 10) % N + 1
+                length += 1
 
 except:
     exit()
