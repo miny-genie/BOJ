f5cf1e989cf8e2fcf800904b365595a4b1417fb6
miny_genie <kjmin2000@naver.com>
2024-01-01 17:28:53 +0900

    BOJ

--- acmicpc_18429.py
+++ acmicpc_18429.py
@@ -7,12 +7,10 @@ input = sys.stdin.readline
 routine, muscle_loss = map(int, input().split())
 muslce_gain = list(map(int, input().split()))
 
-current_muscle = 500
 safe = 0
 
-print(list(permutations(muslce_gain, routine)))
-
 for exercise in permutations(muslce_gain, routine):
+    current_muscle = 500
     for gain in exercise:
         current_muscle = current_muscle + gain - muscle_loss
         if current_muscle < 500: break
