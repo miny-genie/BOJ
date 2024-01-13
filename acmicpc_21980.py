84b15b1482c6ce19cf2de18a6cf8ad420116fd68
miny_genie <kjmin2000@naver.com>
2024-01-14 02:33:55 +0900

    BOJ

--- acmicpc_21980.py
+++ acmicpc_21980.py
@@ -1,29 +1,54 @@
 # ---------- Import ----------
+from collections import Counter
+from itertools import combinations
 import sys
 input = sys.stdin.readline
 
+
 # ---------- Function ----------
-def similar(lst, lst_size):
-    cnt = 0
+def Initialization(words):
+    for word in words:
+        # how many upper alphabet
+        count = 0
+        for char in word:
+            if char.isupper():
+                count += 1
+        upper_cnt[word] = count
+        
+        # what kind of alphabet
+        counter = Counter(word.upper())
+        alpha_cnt[word] = counter
+    return
 
-    for start in range(lst_size - 1):
-        for end in range(start + 1, lst_size):
-            startUpper = sum(c.isupper() for c in lst[start])
-            endUpper = sum(c.isupper() for c in lst[end])
 
-            startTxt = ''.join(sorted(lst[start].upper()))
-            endTxt = ''.join(sorted(lst[end].upper()))
+def findSimilarPair(words):
+    count = 0
+    
+    while words:
+        A = words.pop()
+        pair = set([A])
+        
+        for B in words:
+            if upper_cnt[A] == upper_cnt[B] and alpha_cnt[A] == alpha_cnt[B]:
+                pair.add(B)
+                
+        if len(pair) > 1:
+            length = len(pair)
+            count += length * (length-1) // 2
+            
+        words -= pair
+    
+    return count
 
-            if startUpper == endUpper and startTxt == endTxt:
-                cnt += 1
-
-    return cnt
 
 # ---------- Main ----------
-T = int(input())
+for _ in range(int(input())):
+    word_cnt, word_len = map(int, input().split())
+    words = set(input().rstrip().split())
 
-for _ in range(T):
-    N, K = map(int, input().split())
-    words = list(map(str, input().split()))
-
-    print(similar(words, N))
+    upper_cnt = dict()
+    alpha_cnt = dict()
+    
+    Initialization(words)
+    answer = findSimilarPair(words)
+    print(answer)
