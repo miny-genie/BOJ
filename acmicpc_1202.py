3d5d150476e364e2c7f2d813dfc22788a74ca7fa
kimjimin <kjmin2000@naver.com>
2024-08-23 01:38:15 +0900

    BOJ

---
+++ acmicpc_1202.py
@@ -0,0 +1,27 @@
+import heapq
+from heapq import heapify, heappop, heappush
+from sys import stdin
+input = stdin.readline
+
+FIRST, WEIGHT, VALUE = 0, 0, 1
+
+
+def find_max_price(jewels: heapq, bags: list) -> int:
+    heapify(jewels)
+    max_price, temp = 0, []    
+    for bag in bags:
+        while jewels and bag >= jewels[FIRST][WEIGHT]:
+            heappush(temp, -heappop(jewels)[VALUE])
+        if temp:
+            max_price -= heappop(temp)
+        elif not jewels:
+            break
+    return max_price
+
+
+jewel_count, bag_count = map(int, input().split())
+jewels = [list(map(int, input().split())) for _ in range(jewel_count)]
+bags = sorted(int(input()) for _ in range(bag_count))
+
+max_price = find_max_price(jewels, bags)
+print(max_price)
