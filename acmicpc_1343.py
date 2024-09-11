19d0886aab2d3190fe0742a8f3b5197d45e82d96
kimjimin <kjmin2000@naver.com>
2024-09-11 14:16:29 +0900

    BOJ

---
+++ acmicpc_1343.py
@@ -0,0 +1,23 @@
+import re
+from sys import stdin
+input = stdin.readline
+
+
+def fill_board(board: str) -> str:
+    ret = []
+    for parts in re.findall(r'X+|\.+', board):
+        if parts[0] == ".":
+            ret.append(parts)
+        else:
+            if len(parts) == 1:
+                return -1
+            elif len(parts) % 4 == 0:
+                ret.append("A" * len(parts))
+            else:
+                ret.append("A" * (len(parts)-2) + "BB")
+    return ''.join(ret)
+
+
+board = input().rstrip()
+board = fill_board(board)
+print(board)
