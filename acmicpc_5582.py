6a5dd75a0adcdec7ad56ffc2dbfc54ce5d4ec716
kimjimin <kjmin2000@naver.com>
2025-03-22 14:58:19 +0900

    25.03.22, 공통 부분 문자열(dp)

--- acmicpc_5582.py
+++ acmicpc_5582.py
@@ -5,15 +5,17 @@ input = stdin.readline
 def longest_common_substring(word1: str, word2: str) -> int:
     len1, len2 = len(word1), len(word2)
     dp = [0] * (len2 + 1)
+    max_len = 0
     
     for i in range(1, len1 + 1):
         for j in range(len2, 0, -1):
             if word1[i - 1] == word2[j - 1]:
-                dp[j] = dp[j-1] + 1
+                dp[j] = dp[j - 1] + 1
+                max_len = max(max_len, dp[j])
             else:
                 dp[j] = 0
         
-    return max(dp)
+    return max_len
 
 
 word1 = input().rstrip()
