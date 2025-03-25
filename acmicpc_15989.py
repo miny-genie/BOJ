5a4967e8e75f0bc0cf5b4bc84157a3f3e43b9c73
kimjimin <kjmin2000@naver.com>
2025-03-25 17:56:49 +0900

    25.03.25, 1, 2, 3 더하기 4(dp)

--- acmicpc_15989.py
+++ acmicpc_15989.py
@@ -3,15 +3,14 @@ input = stdin.readline
 
 
 def generate_case() -> list[int]:
-    dp = [1] * 10001
+    dp = [1] * (10000 + 1)
     for coin in [2, 3]:
-        for i in range(coin, 10001):
+        for i in range(coin, 10000 + 1):
             dp[i] += dp[i - coin]
     return dp
 
 
-nums = [int(input()) for _ in range(int(input()))]
 dp = generate_case()
-
-print(*[dp[num] for num in nums], sep='\n')
-
+for _ in range(int(input())):
+    idx = int(input())
+    print(dp[idx])
